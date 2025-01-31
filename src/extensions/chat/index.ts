2ea4596e782028b5e45036731d74a0ad189b7bdb
Fizitzfux <66255957+fizitzfux@users.noreply.github.com>
2025-01-31 12:05:26 +0000

    Redo chat using SSE

--- src/extensions/chat/index.ts
+++ src/extensions/chat/index.ts
@@ -1,60 +1,85 @@
 import { ExtensionBase } from "../../modules.ts"
 
+type message = {user: {name: any, pfp_code: any}, time: any, content: any}
+
 export default class extends ExtensionBase {
     override name = 'chat'
     override title = 'Chat'
 
-    messages: {user: {name: any, pfp_code: any}, time: any, content: any}[] = [{
-        user: {name:'SYSTEM',pfp_code:'seed=SYSTEM'},
-        time:(new Date()).toLocaleTimeString('en-US', {hour12: false}),
-        content: 'Welcome to the chatroom!'
-    }]
-    last_got_id: {[user_id: number]: number} = {}
+    private MessageStore = class {
+        messages: message[] = [{
+            user: {name:'SYSTEM',pfp_code:'seed=SYSTEM'},
+            time:(new Date()).toLocaleTimeString('en-US', {hour12: false}),
+            content: 'Welcome to the chatroom!'
+        }]
+        onPushListeners: Set<(msg: message) => void> = new Set()
+    
+        push(ctx: Context, content: string) {
+            const message: message = {
+                user: {
+                    name: ctx.context.user?.name,
+                    pfp_code: `${ctx.context.dicebear_host}?${ctx.context.user?.pfp_code}`,
+                },
+                time: (new Date()).toLocaleTimeString('en-US', {hour12: false}),
+                content,
+            }
+            this.messages.push(message)
+            this.onPushListeners.forEach((listener) => listener(message))
+        }
+    }
+    message_store = new this.MessageStore()
 
     override handle: Extension['handle'] = (ctx) => {
-        var location = ctx.path.shift()
+        const location = ctx.path.shift()
 
-        const user_id = ctx.context.user?.id as number
+        switch (location) {
+            case '':
+            case undefined: {
+                if (ctx.data && ctx.data.form.message) {
+                    const message = ctx.data.form.message.substring(0,255)
+                    this.message_store.push(ctx, message)
+                }
+                ctx.context.chat = this.message_store.messages
+                return this.return_html(ctx, 'index')
+            }
+            case 'history': {
+                // Should at some point return history by request
+                return this.return_data(ctx, JSON.stringify({messages: this.message_store.messages}))
+            }
+            case 'new_message_event': {
+                const {req, res} = ctx
 
-        if (!location) {
-            if (ctx.data && ctx.data.form.message) {
-                var message = ctx.data.form.message.substring(0,255)
-                var now = (new Date()).toLocaleTimeString('en-US', {hour12: false})
-                this.messages.push({
-                    user: {
-                        name: ctx.context.user?.name,
-                        pfp_code: ctx.context.user?.pfp_code,
-                    },
-                    time: now,
-                    content: message,
+                res.writeHead(200, {
+                    'Content-Type': 'text/event-stream',
+                    'Cache-Control': 'no-cache',
+                });
+                
+                let counter = 0;
+                
+                // Send a message on connection
+                res.write('event: connected\n');
+                res.write(`data: null\n`);
+                res.write(`id: ${counter}\n\n`);
+                counter += 1;
+                
+                const listener = (msg: message) => {
+                    res.write('event: message\n');
+                    res.write(`data: ${JSON.stringify(msg)}\n`);
+                    res.write(`id: ${counter}\n\n`);
+                    counter += 1;
+                }
+                this.message_store.onPushListeners.add(listener)
+                
+                // Close the connection when the client disconnects
+                req.on('close', () => {
+                    this.message_store.onPushListeners.delete(listener)
+                    res.end('OK')
                 })
+                return
             }
-            ctx.context.chat = this.messages
-            this.last_got_id[user_id] = this.messages.length
-            return this.return_html(ctx, 'index')
-        }
-        else if (location == 'getnew') {
-            var part = this.last_got_id.hasOwnProperty(user_id) ? this.last_got_id[user_id] : 0
-            this.last_got_id[user_id] = this.messages.length
-            return this.return_data(ctx, `{"messages":${JSON.stringify(this.messages.slice(part))}}`)
-        }
-        else if (location == 'postmessage') {
-            if (ctx.data && ctx.data.form.message) {
-                var message = ctx.data.form.message.substring(0,255)
-                var now = (new Date()).toLocaleTimeString('en-US', {hour12: false})
-                this.messages.push({
-                    user: {
-                        name: ctx.context.user?.name,
-                        pfp_code: ctx.context.user?.pfp_code,
-                    },
-                    time: now,
-                    content: message,
-                })
+            default: {
+                return this.return_file(ctx, location)
             }
-            return this.return(ctx)
-        }
-        else {
-            return this.return_file(ctx, location)
         }
     }
 }
