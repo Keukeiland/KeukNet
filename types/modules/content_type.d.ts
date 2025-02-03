aa1295687351d0cc4df60257b4d674cda1230de6
Fizitzfux <66255957+fizitzfux@users.noreply.github.com>
2025-02-03 18:29:21 +0000

    Fix content types

--- types/modules/content_type.d.ts
+++ types/modules/content_type.d.ts
@@ -1,19 +1,19 @@
 declare interface ContentType {
-    readonly HTML: HttpHeader
-    readonly ASCII: HttpHeader
-    readonly TXT: HttpHeader
-    readonly JSON: HttpHeader
-    readonly ICO: HttpHeader
-    readonly CSS: HttpHeader
-    readonly GIF: HttpHeader
-    readonly JPG: HttpHeader
-    readonly JS: HttpHeader
-    readonly PNG: HttpHeader
-    readonly MD: HttpHeader
-    readonly XML : HttpHeader
-    readonly SVG: HttpHeader
-    readonly WEBMANIFEST: HttpHeader
-    readonly MP3: HttpHeader
-    readonly EXE: HttpHeader
-    readonly PY: HttpHeader
+    readonly html: HttpHeader
+    readonly ascii: HttpHeader
+    readonly txt: HttpHeader
+    readonly json: HttpHeader
+    readonly ico: HttpHeader
+    readonly css: HttpHeader
+    readonly gif: HttpHeader
+    readonly jpg: HttpHeader
+    readonly js: HttpHeader
+    readonly png: HttpHeader
+    readonly md: HttpHeader
+    readonly xml : HttpHeader
+    readonly svg: HttpHeader
+    readonly webmanifest: HttpHeader
+    readonly mp3: HttpHeader
+    readonly exe: HttpHeader
+    readonly py: HttpHeader
 }
